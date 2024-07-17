f4019d11cd207b988e412fa3dcc05c3929603532
saloni-s6 <122854015+saloni-s6@users.noreply.github.com>
2024-07-17 12:52:44 +0000

    day 1

---
+++ first.js
@@ -0,0 +1,12 @@
+const {add , multiply} =
+
+//  Global variables
+
+console.log(__dirname) // workspaces/NodeIntro
+console.log(__filename); // /workspaces/NodeIntro/first.js
+
+// Commonjs module
+
+// every file is module
+
+console.log(add(2, 3));
